4658c3780f42f6ed1deeb569a9796132f47ce9ff
Steve Rayner <srayner02@gmail.com>
2016-06-22 21:48:39 +0100

    Added machine doctrine entity.

--- module/Titanium/src/Titanium/Entity/Machine.php
+++ module/Titanium/src/Titanium/Entity/Machine.php
@@ -2,9 +2,54 @@
 
 namespace Titanium\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/** @ORM\Entity
+  * @ORM\Table(name="machine")
+  */
 class Machine extends AbstractEntity
 {
+    /** @ORM\Column(type="string") */
     protected $name;
-    protected $description;
+    
+    /** @ORM\Column(type="string", name="machine_name") */
+    protected $type;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="Plant", inversedBy="plants")
+     * @ORM\JoinColumn(name="plant_id", referencedColumnName="id")
+     */
     protected $plant;
+    
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function getType()
+    {
+        return $this->type;
+    }
+
+    function getPlant()
+    {
+        return $this->plant;
+    }
+
+    function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    function setPlant($plant)
+    {
+        $this->plant = $plant;
+    }
+
+
 }
